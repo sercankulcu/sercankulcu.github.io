3163a293a2b9b51dd0614bc0857ea99f11ed12f7
Sercan Külcü <sercankulcu@gmail.com>
2024-03-24 23:01:46 +0300

    update

---
+++ mantra/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        heading: "Welcome!",
+        text: "Are you ready for a journey designed to calm the thoughts that arise during meditation and bring peace to your mind? Join us as we embark on an exploration in harmony with the natural essence of meditation, allowing you to flow effortlessly with your inner rhythm.",
+    },
+    {
+        heading: "Mantra: The Silent Dance of Thoughts",
+        text: "At the core of meditation are mantras, not just word combinations but also keys that enable you to observe your thoughts. The brain adapts to the tranquil and repetitive frequency of the mantra, resembling a dance of harmony rather than force. Here, you will travel within your inner world with the magical rhythm of mantras, observing the serene dance of thoughts.",
+    },
+    {
+        heading: "Observation of Thoughts and Discovery of Simplicity",
+        text: "Meditation is not just about achieving a state but understanding the cyclical nature of thoughts and discovering the beauty of simplicity. Beyond thoughts, find balance in harmony with your inner rhythm. Mantras should be used as a guide directing you towards the nature of meditation.",
+    },
+    {
+        heading: "Journey to Harmony Without Force",
+        text: "Meditation with mantras invites you on an adventure to align with your natural frequency. Discover a calm and gentle path beyond mental clutter, allowing you to peacefully traverse beyond your thoughts. Remember, it's not a race; it's a gentle guidance for you to comfortably progress in your inner exploration.",
+    },
+    {
+        heading: "Meet Yourself, Dance with Your Thoughts",
+        text: "Meditation with mantras provides a key to meet yourself. Quietly observe your thoughts, find your rhythm, and explore the beauty of meditation. Each repetition brings you a step closer to your inner peace.",
+    },
+];
