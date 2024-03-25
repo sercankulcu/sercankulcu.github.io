d0f9e4edf9ec28c37412444f73b907215d67dc1a
Sercan Külcü <sercankulcu@gmail.com>
2024-03-25 13:23:17 +0300

    Update service-worker.js

--- mantra/service-worker.js
+++ mantra/service-worker.js
@@ -9,8 +9,8 @@ const urlsToCache = [
   '/styles.css',
   '/script.js',
   '/data.js',
-  '/assets/media/please-calm-my-mind.mp3',
-  '/assets/icons/icon-512.png'
+  //'/assets/media/please-calm-my-mind.mp3',
+  //'/assets/icons/icon-512.png'
   // Add more URLs to cache as needed
 ];
 
