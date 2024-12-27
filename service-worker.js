5e41e7449689147b744296abe0bf39a3e26db98f
Sercan Külcü <sercankulcu@gmail.com>
2024-12-27 20:11:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -33,34 +33,20 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Event: Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      // Cache hit - return the cached response
-      if (response) {
-        return response;
-      }
-
-      // Clone the request since it's a one-time use
-      const fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then(response => {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-
-        // Clone the response since it's a one-time use
+    fetch(event.request)
+      .then(response => {
+        // Yeni cevap cache'e ekleniyor
         const responseToCache = response.clone();
-
-        // Open the cache and add the new response
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, responseToCache);
         });
-
         return response;
-      });
-    })
+      })
+      .catch(() => {
+        // Eğer fetch başarısız olursa cache'den getir
+        return caches.match(event.request);
+      })
   );
 });
