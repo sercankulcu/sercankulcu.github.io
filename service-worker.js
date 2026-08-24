85f80e4783b45bdccf3d0a624c6044fa4fecd043
Sercan Külcü <sercankulcu@gmail.com>
2026-08-24 14:27:03 +0300

    .

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "my-pwa-sercan-cache-";
-const CACHE_VERSION = "v29";
+const CACHE_VERSION = "v30";
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
